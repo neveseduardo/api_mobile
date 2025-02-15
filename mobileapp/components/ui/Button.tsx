a5b944d5eb20dc56224335fa7265992278db11eb
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2025-02-15 12:30:18 -0300

    feat: Button + Input

---
+++ mobileapp/components/ui/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+const Button = () => {
+	return (
+		<ThemedView style={styles.container}>
+			<ThemedText>Button</ThemedText>
+		</ThemedView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+
+export default Button;
